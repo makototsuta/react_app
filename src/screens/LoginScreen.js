d1e5779cf86d88559ff94c79b587a551d7310b7c
makoto tsuta <makoto1850@gmail.com>
2020-10-25 20:13:05 +0900

    Signup and Login with firebase

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import firebase from 'firebase';
 
 
 class LoginScreen extends React.Component {
@@ -9,7 +10,14 @@ class LoginScreen extends React.Component {
   }
 
   handleSubmit() {
-    //{ this.props.navigation.navigate('Home')}
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then((result) => {
+      console.log('success!', result.user);
+      this.props.navigation.navigate('Home');
+    })
+    .catch((error) => {
+      console.log('error', error);
+    });
   }
 
   render() {
@@ -33,7 +41,7 @@ class LoginScreen extends React.Component {
           autoCapitalize="none"
           autoCorrect={false}
           placeholder="Password"
-          secureTextEntrydddd
+          secureTextEntry
         />
         <TouchableHighlight style={styles.button} onPress={this.handleSubmit.bind(this)} >
           <Text style={styles.buttonTitle}>ログインする</Text>
