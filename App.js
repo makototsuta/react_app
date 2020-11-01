e4e0430cbab921f7f4899ec1c0a7833345c3966c
makoto tsuta <makoto1850@gmail.com>
2020-11-01 21:19:08 +0900

    Store memos with cloud store

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import firebase from 'firebase';
 
 import ENV from './env.json';
+
+require("firebase/firestore");
 
 const firebaseConfig = {
   apiKey:             ENV.FIREBASE_API_KEY,
@@ -29,6 +32,7 @@ const App = createStackNavigator({
   Home:       { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
+  MemoCreate: { screen: MemoCreateScreen },
 }, {
   defaultNavigationOptions: {
     headerTitle: 'Memot',
